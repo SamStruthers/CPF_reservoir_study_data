bdf7c96f0f0de29106cb72630196ebe54a3e96ea
SamStruthers <sam.struthers2000@gmail.com>
2023-10-17 11:42:59 -0600

    fixed link for download, finalized example plot

--- scripts/demo.R
+++ scripts/demo.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(arrow)
 
 # Download the entire folder from Zenodo DOI as a zipped folder in your working directory
-download.file('https://zenodo.org/record/8308733/files/rossyndicate/CPF_reservoir_study_data-latest.zip?download=1',destfile = 'ross_cpf.zip') 
+download.file('https://zenodo.org/records/8308733/files/rossyndicate/CPF_reservoir_study_data-latest.zip?download=1',destfile = 'ross_cpf.zip') 
 # unzip this file
 unzip('ross_cpf.zip') 
  # Grab the name of the download file from current R project   
@@ -15,11 +15,11 @@ CPF_download_file <- list.files() %>%
 # For the current release that file is `CPF_reservoir_chemistry_up_to_071023.csv`
 most_recent_chem <- read_csv_arrow(paste0(CPF_download_file,'/data/cleaned/CPF_reservoir_chemistry_up_to_071023.csv'))
 
-#Example plot of NO3 over time at three sites
-no3_ex_plot <- most_recent_chem %>% 
-  filter(site_code %in% c("JOEI","CBRR", "JWC" )& Year > 2020)%>%
+#Example plot of NO3 over time at four sites
+no3_example_plot <- most_recent_chem %>% 
+  filter(site_code %in% c("JOEI","CBRI", "CHD", "JWC" )& Year > 2020)%>%
   ggplot(aes(x = Date, y = NO3, color = site_code)) + 
   geom_point(size = 2)+
     labs(x = "Date", y = "NO3 (mg/L)")+
     theme_bw(base_size = 15)
-no3_ex_plot
+no3_example_plot
